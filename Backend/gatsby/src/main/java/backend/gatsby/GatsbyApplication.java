6dbd7a9dcb6b6d36eecce0077d4b305565a78554
augustom <augustom@iastate.edu>
2020-09-01 07:35:51 -0500

    Created an initial structure of a Spring Boot App for Gatsby

---
+++ Backend/gatsby/src/main/java/backend/gatsby/GatsbyApplication.java
@@ -0,0 +1,13 @@
+package backend.gatsby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatsbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatsbyApplication.class, args);
+	}
+
+}
---
+++ Backend/gatsby/src/test/java/backend/gatsby/GatsbyApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.gatsby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatsbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
