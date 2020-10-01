b3690b614aca81ae4fa05a6e84f4e1179fb534e4
augustom <augustom@iastate.edu>
2020-10-01 06:45:16 -0500

    Initial test with delete request

--- Backend/gatsby/src/main/java/backend/gatsby/Controller.java
+++ Backend/gatsby/src/main/java/backend/gatsby/Controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,4 +52,9 @@ public class Controller {
 		return db.findById(id).get();
 	}
 	
+	@DeleteMapping("/attendee/{id}")
+	String deleteUser(@PathVariable Integer id) {
+		db.delete(db.findById(id).get());
+		return "Deleted " + id;
+	}
 }
