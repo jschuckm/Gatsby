f2d12c8af5b411e6388b1a67d4f0de0d1a68788a
Constantine Mantas <cmantas@iastate.edu>
2020-10-26 14:12:53 -0500

    Demo3 up to date

---
+++ Frontend/Gatsby/app/src/main/java/com/example/gatsby/ui/eventListing/EventViewFragment.java
@@ -0,0 +1,129 @@
+package com.example.gatsby.ui.eventListing;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.gatsby.MyApplication;
+import com.example.gatsby.R;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventViewFragment extends Fragment {
+    private EventViewModel eventViewModel;
+
+    String j = "0";
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        eventViewModel =
+                ViewModelProviders.of(this).get(EventViewModel.class);
+        final View root = inflater.inflate(R.layout.fragment_eventview, container, false);
+        Bundle bundle = getArguments();
+        if(bundle != null){
+             j = bundle.getString("key");
+        }
+
+        final Integer i = Integer.parseInt(j);
+
+        final TextView EventName = (TextView) root.findViewById(R.id.EventName);
+        final TextView size = (TextView) root.findViewById(R.id.size);
+        final TextView date = (TextView) root.findViewById(R.id.date);
+        final TextView fee = (TextView) root.findViewById(R.id.fee);
+
+        final TextView location2 = (TextView) root.findViewById(R.id.location2);
+        final TextView hostname = (TextView) root.findViewById(R.id.hostname);
+
+
+
+        Button Apply = (Button) root.findViewById(R.id.apply);
+        Button Back = (Button) root.findViewById(R.id.back);
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EventListingFragment e = new EventListingFragment();
+                FragmentManager manager = getFragmentManager();
+                manager.beginTransaction().replace(R.id.nav_host_fragment, e).commit();
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(root.getContext());
+                try {
+                    String url ="http://coms-309-mc-07.cs.iastate.edu:8080/events";
+                    JSONObject object = new JSONObject();
+                    JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            try {
+                                JSONArray jsonArray = new JSONArray(response.toString());
+
+                                JSONObject first = jsonArray.getJSONObject(i);
+                                String pub = "";
+                                if(first.get("isPublic").toString().equals("true")){
+                                    pub = "Public";
+                                }
+                                else{
+                                    pub = "Private";
+                                }
+
+                                EventName.setText(first.get("name").toString());
+                                size.setText(first.get("capacity").toString());
+                                date.setText(first.get("date").toString());
+                                fee.setText(first.get("fee").toString());
+                                location2.setText(first.get("address").toString());
+                                hostname.setText(pub);
+
+                            }
+                            catch(Exception e){
+                                System.out.println(e);
+                                System.out.println("ERROR");
+                            }
+
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            System.out.println(error);
+                            System.out.println("OTHER ERROR");
+                        }
+                    }){
+                        @Override
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            HashMap<String, String> headers = new HashMap<String, String>();
+                            System.out.println(MyApplication.getUser().getAuthToken());
+                            headers.put("Authorization", MyApplication.getUser().getAuthToken());
+
+                            return headers;
+                        }};
+                    requestQueue.add(jsonArrayRequest);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+
+        return root;
+    }
+
+
+}
---
+++ Frontend/Gatsby/app/src/test/java/com/example/gatsby/MockitoTests.java
@@ -0,0 +1,106 @@
+package com.example.gatsby;
+
+import com.example.gatsby.ui.eventListing.EventListingFragment;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoTests {
+
+    @Mock
+    private JSONObject mockJSON;
+
+    @Mock
+    private JSONObject mockSecond;
+
+    @Mock
+    private JSONObject mockThird;
+
+    @Mock
+    private JSONObject mockFourth;
+
+    @Test
+    public void testParseJson() throws JSONException {
+        EventListingFragment event = new EventListingFragment();
+
+        when(mockJSON.getString("name")).thenReturn("test");
+        when(mockSecond.getString("name")).thenReturn("test2");
+        when(mockThird.getString("name")).thenReturn("test3");
+        when(mockFourth.getString("name")).thenReturn("test4");
+
+
+        String sum = event.parseJson(mockJSON,mockSecond,mockThird,mockFourth);
+
+        System.out.println(sum);
+
+        assertEquals("testtest2test3test4",sum);
+    }
+
+    @Test
+    public void testParseFee() throws JSONException {
+        EventListingFragment event = new EventListingFragment();
+
+        when(mockJSON.getDouble("fee")).thenReturn(1.1);
+        when(mockSecond.getDouble("fee")).thenReturn(2.0);
+        when(mockThird.getDouble("fee")).thenReturn(1.5);
+        when(mockFourth.getDouble("fee")).thenReturn(0.4);
+
+
+        String sum = "" + (float) event.parseFee(mockJSON,mockSecond,mockThird,mockFourth);
+        String test = "" +sum;
+
+
+        assertEquals("5.0", sum);
+    }
+
+    @Test
+    public void testParseUser() throws JSONException {
+        EventListingFragment event = new EventListingFragment();
+
+        when(mockJSON.getString("age")).thenReturn("5");
+        when(mockJSON.getString("name")).thenReturn("Bob");
+        when(mockJSON.getString("address")).thenReturn("Ames");
+        when(mockJSON.getString("email")).thenReturn("cmantas@iastate.edu");
+        when(mockJSON.getString("rating")).thenReturn("1");
+
+
+
+
+       String sum = event.parseUser(mockJSON);
+
+        System.out.println(sum);
+
+        assertEquals("5BobAmescmantas@iastate.edu1",sum);
+    }
+
+    @Test
+    public void testParseEvent() throws JSONException {
+        EventListingFragment event = new EventListingFragment();
+
+        when(mockJSON.getString("eventName")).thenReturn("poolparty");
+        when(mockJSON.getString("eventLocation")).thenReturn("Ames");
+        when(mockJSON.getString("capacity")).thenReturn("3");
+        when(mockJSON.getString("fee")).thenReturn("1");
+        when(mockJSON.getString("isPublic")).thenReturn("true");
+
+
+
+
+        String sum = event.parseEvent(mockJSON);
+
+        System.out.println(sum);
+
+        assertEquals("poolpartyAmes31true",sum);
+    }
+
+
+
+}
