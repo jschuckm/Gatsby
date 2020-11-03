b9ab796caeda65dd0fd63a69338fd7d5f9a67944
augustom <augustom@iastate.edu>
2020-11-03 07:28:01 -0600

    Adding swagger ui for the API

---
+++ Backend/gatsby/src/main/java/backend/gatsby/SpringFoxConfig.java
@@ -0,0 +1,38 @@
+package backend.gatsby;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.builders.ApiInfoBuilder;
+
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {                                    
+
+	@Bean
+	public Docket myDocket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("myProject"))
+				.paths(PathSelectors.any())
+				.build();
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("gatsby_server")
+				.description("Hi, welcome to the backend of the Coms309 mc_07 Gatsby project, here is the API document!")
+				.build();
+	}
+
+}
